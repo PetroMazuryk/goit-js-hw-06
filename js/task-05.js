62eb21c11215508e742d684bcbdade72a04d1fee
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2022-11-26 01:11:57 +0200

    changed ""

--- js/task-05.js
+++ js/task-05.js
@@ -1,25 +1,25 @@
-// const refs = {
-//   input: document.querySelector("#name-input"),
-//   output: document.querySelector("#name-output"),
-// };
+const refs = {
+  input: document.querySelector("#name-input"),
+  output: document.querySelector("#name-output"),
+};
 
-// const blankLine = "Anonymous";
-// const onInputWrite = (event) => {
-//   if (event.currentTarget.value === blankLine) {
-//     return (refs.output.textContent = blankLine);
-//   }
-//   return (refs.output.textContent = event.currentTarget.value);
-// };
+const blankLine = "Anonymous";
+const onInputWrite = (event) => {
+  if (event.currentTarget.value === "") {
+    return (refs.output.textContent = blankLine);
+  }
+  return (refs.output.textContent = event.currentTarget.value);
+};
 
-// refs.input.addEventListener("input", onInputWrite);
+refs.input.addEventListener("input", onInputWrite);
 
 // // Варіант 2 Якщо інпут порожній, у спані  відображатися рядок "Anonymous".
 
-const inputEL = document.querySelector("#name-input");
-const spanEl = document.querySelector("#name-output");
+// const inputEL = document.querySelector("#name-input");
+// const spanEl = document.querySelector("#name-output");
 
-inputEL.addEventListener("input", (event) => {
-  return event.currentTarget.value
-    ? (spanEl.textContent = event.currentTarget.value)
-    : (spanEl.textContent = "Anonymos");
-});
+// inputEL.addEventListener("input", (event) => {
+//   return event.currentTarget.value
+//     ? (spanEl.textContent = event.currentTarget.value)
+//     : (spanEl.textContent = "Anonymos");
+// });
